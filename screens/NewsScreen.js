2f9f9faebbee8008fdc37ab49771d70dec3352eb
Rdean-Dev <rdean.dev@gmail.com>
2026-09-08 23:50:36 -0400

    Add placeholder routes for main app screens

---
+++ screens/NewsScreen.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Text, StyleSheet, View, ScrollView } from 'react-native';
+
+export default function NewsScreen() {
+    return <Text>News</Text>;
+}
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Text, StyleSheet, View, ScrollView } from 'react-native';
+
+export default function SettingsScreen() {
+    return <Text>Settings</Text>;
+}
